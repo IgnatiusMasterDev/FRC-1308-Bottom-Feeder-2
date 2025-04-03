66cd7570b1669158d040b436874e556e6d13364f
SIHS-Robotics <sihsrobotics@ignatius.edu>
2025-04-03 13:29:51 -0400

    javadoc for ToggleArmsCommand

--- src/main/java/frc/robot/commands/ToggleArmsCommand.java
+++ src/main/java/frc/robot/commands/ToggleArmsCommand.java
@@ -3,6 +3,9 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.grabber.ArmsSubsystem;
 
+/**
+ * Raises or lowers the robot's arms.
+ */
 public class ToggleArmsCommand extends Command {
     
     private final ArmsSubsystem arms;
