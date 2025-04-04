38a2530c697f5e90ef97c63e09e2cb3495ff8a6d
SIHS-Robotics <sihsrobotics@ignatius.edu>
2025-04-04 18:19:25 -0400

    constant changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -190,7 +190,7 @@ public class RobotContainer {
         // Pass through these two interior waypoints, making an 's' curve path
         List.of(new Translation2d(1, 0), new Translation2d(2, 0)),
         // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(2.55, 0, new Rotation2d(0)),
+        new Pose2d(2.5, 0, new Rotation2d(0)),
         //2.7 is roughly 16 ft
         config);
 
