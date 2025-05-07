d32609991b8e4f6983517af20dc801e41017e51d
SIHS-Robotics <sihsrobotics@ignatius.edu>
2025-05-07 15:51:05 -0400

    complete climber subsystem

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -14,8 +14,16 @@ public final class ClimberSubsystem extends SubsystemBase{
     private final NetworkTableInstance networkTables = NetworkTableInstance.getDefault();
     private final NetworkTable table = networkTables.getTable("climber");
 
+    private final double tightenLimit = 137.453;
+    private final double loosenLimit = 8.741;
+    private final double percentileFactor = (tightenLimit - loosenLimit);
+
     private final DoublePublisher positionPublisher = table
-        .getDoubleTopic("position")
+        .getDoubleTopic("position (rot)")
+        .publish();
+
+    private final DoublePublisher positionPercentilePublisher = table
+        .getDoubleTopic("position (percentile)")
         .publish();
 
     public ClimberSubsystem() {
@@ -25,7 +33,39 @@ public final class ClimberSubsystem extends SubsystemBase{
     @Override
     public void periodic() {
         // Update the position publisher with the current position of the motor
-        positionPublisher.set(getPosition());
+        positionPublisher.set(motor.getPosition().getValueAsDouble());
+        positionPercentilePublisher.set(getPosition());
+    }
+
+    /**
+     * Loosens the climber.
+     * 
+     * @param speed between 0 and 1.
+     */
+    public void loosen(double speed) {
+        if (!fullyLoosened()) {
+            setSpeed(-speed);
+        } else {
+            stop();
+        }
+    }
+
+    /**
+     * Tightens the climber.
+     * 
+     * @param speed between 0 and 1.
+     */
+    public void tighten(double speed) {
+        if (!fullyTightened()) {
+            setSpeed(speed);
+        } else {
+            stop();
+        }
+    }
+
+    /** Stops the climber. */
+    public void stop() {
+        motor.stopMotor();
     }
 
     /**
@@ -38,11 +78,29 @@ public final class ClimberSubsystem extends SubsystemBase{
     }
 
     /**
-     * Returns the position of the climber.
+     * Returns whether the climber is fully tightened (pulled back).
      * 
-     * @return the position of the climber
+     * @return whether the climber is fully released.
+     */
+    public boolean fullyTightened() {
+        return getPosition() >= 1;
+    }
+
+    /**
+     * Returns whether the climber is fully loosened (released).
+     * 
+     * @return whether the climber is fully released.
+     */
+    public boolean fullyLoosened() {
+        return getPosition() <= 0;
+    }
+
+    /**
+     * Returns the position of the climber as a percentile of the loose-tight range.
+     * 
+     * @return the position of the climber as a percentile.
      */
     public double getPosition() {
-        return motor.getPosition().getValueAsDouble();
+        return (motor.getPosition().getValueAsDouble() - loosenLimit) / percentileFactor; 
     }
 }
